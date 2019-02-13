fec4398ee208ab58af8eeaa1edaddc9724580eba
MarekDana <43402311+MarekDana@users.noreply.github.com>
2019-02-13 09:22:14 +0100

    dsad
    sadsad

--- src/index.js
+++ src/index.js
@@ -1,50 +1,36 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-class App extends Component {
-  
+export default class App extends React.Component {
   state = {
     beers: []
+  };
+  constructor() {
+    super();
+    this.getBeers();
   }
-  
-  fetchBeers = (param) => {
-    fetch(`https://randomuser.me/api/${param}`)
-      .then((resp) => resp.json())
-      .then(function (data) {
-        let osoba = data.results;
-      
 
-        console.log(`${osoba.name.first} ${osoba.name.last}`);
-        
-  });
-  
-  
+  getBeers() {
+    fetch(`http://private-9c94be-namelist3.apiary-mock.com/questions`)
+      .then(data => data.json())
+      .then(beers => {
+        this.setState({ beers: beers.arrNames });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
-  
-  render(){
-    let beers = this.state.beers.map(beer => <p style={styles}> {beer.name} </p>);
-    return(
-        <div>
-          <button onClick={this.fetchBeers}> 
-              All 
-          </button>
-          <button onClick={() => this.fetchBeers("food=taco") }> 
-              Beers for taco!
-          </button>
-          
-          <section>
-            { beers }
-          </section>
-        </div>
-    );
+
+  render() {
+    let beerNames = this.state.beers.map(beer => (
+      <li>
+        <strong> {beer.name}</strong>
+      </li>
+    ));
+    return <div> {beerNames}</div>;
+    //<div> {JSON.stringify(this.state.beers)}</div>;
   }
 }
 
-const styles = {
-  font: '100%/1.5 monospace',
-  color: '#222',
-  fontWeight: 'bold'
-}
-
-
-render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
